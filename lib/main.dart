47264bb51917904057ae7429d628ce9cfef57dda
Alexander Teusz <teusz.alexander@gmail.com>
2020-04-13 14:14:08 +0200

    removed provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,15 @@
 //import 'package:cognigy_flutter_client/widgets/message.dart';
 import 'package:cognigy_flutter_client/helper/message_helper.dart';
 import 'package:cognigy_flutter_client/models/message_model.dart';
-import 'package:cognigy_flutter_client/providers/message_provider.dart';
-import 'package:cognigy_flutter_client/widgets/main_appBar.dart';
-import 'package:cognigy_flutter_client/widgets/messages.dart';
 import 'package:flutter/material.dart';
+import 'package:cognigy_flutter_client/widgets/configuration_dialog.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 // Import required files for Cognigy.AI connection
 import 'package:flutter_simple_dependency_injection/injector.dart';
 import 'package:cognigy_flutter_client/cognigy/app_initializer.dart';
 import 'package:cognigy_flutter_client/cognigy/dependency_injection.dart';
 import 'package:cognigy_flutter_client/cognigy/socket_service.dart';
-import 'package:provider/provider.dart';
 
 // Create Injector
 Injector injector;
@@ -20,9 +18,7 @@ void main() async {
   DependencyInjection().initialise(Injector.getInjector());
   injector = Injector.getInjector();
   await AppInitializer().initialise(injector);
-  // Run App with Provider
-  runApp(ChangeNotifierProvider<MessageProvider>(
-      create: (_) => MessageProvider(), child: MyApp()));
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -46,17 +42,49 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  FocusNode focusNode;
+  List messages;
   double height, width;
+  TextEditingController textController;
+  ScrollController scrollController;
+  Message cognigyMessage;
 
   final SocketService socketService = injector.get<SocketService>();
-  FocusNode focusNode;
 
   @override
   void initState() {
-    super.initState();
+    //Initializing the message list
+    messages = List();
 
-    socketService.createSocketConnection();
+    //Initializing the TextEditingController and ScrollController
+    textController = TextEditingController();
+    scrollController = ScrollController();
+
     focusNode = FocusNode();
+
+    // Connect to Cognigy.AI Socket.IO Endpoint
+    socketService.createSocketConnection();
+
+    try {
+        socketService.socket.on('output', (cognigyResponse) {
+        // process the cognigy output message
+        cognigyMessage = processCognigyMessage(cognigyResponse);
+
+        if (cognigyMessage != null) {
+          this.setState(
+              () => messages.add({'message': cognigyMessage, 'sender': 'bot'}));
+
+          //Scrolldown the list to show the latest message
+          scrollController.animateTo(
+            scrollController.position.maxScrollExtent,
+            duration: Duration(milliseconds: 600),
+            curve: Curves.ease,
+          );
+        }
+      });
+    } catch (error) {}
+
+    super.initState();
   }
 
   @override
@@ -67,7 +95,7 @@ class _ChatPageState extends State<ChatPage> {
     super.dispose();
   }
 
-  Widget buildChatInput(MessageProvider messageProvider) {
+  Widget buildChatInput() {
     return Container(
       width: width * 0.7,
       constraints: BoxConstraints(minWidth: width * 0.7),
@@ -87,24 +115,29 @@ class _ChatPageState extends State<ChatPage> {
           autocorrect: true,
           enableSuggestions: true,
           onChanged: (value) {
-            messageProvider.setUserInputText(value);
+            setState(() {
+              textController.text = value;
+            });
           },
           onEditingComplete: () {
             //Check if the textfield has text or not
-            if (messageProvider.getUserInputText.isNotEmpty) {
-              socketService.sendMessage(messageProvider.getUserInputText);
+            if (textController.text.isNotEmpty) {
+              socketService.sendMessage(textController.text);
 
-              messageProvider.addMessage(
-                  new Message('text', messageProvider.getUserInputText, null),
-                  'user');
+              setState(() {
+                messages.add({
+                  'message': new Message('text', textController.text, null),
+                  'sender': 'user'
+                });
+              });
 
-              messageProvider.setUserInputText('');
+              textController.text = '';
 
               focusNode.unfocus();
 
               //Scrolldown the list to show the latest message
-              messageProvider.getScrollController.animateTo(
-                messageProvider.getScrollController.position.maxScrollExtent,
+              scrollController.animateTo(
+                scrollController.position.maxScrollExtent,
                 duration: Duration(milliseconds: 600),
                 curve: Curves.ease,
               );
@@ -113,13 +146,13 @@ class _ChatPageState extends State<ChatPage> {
           decoration: InputDecoration.collapsed(
             hintText: 'Send a message...',
           ),
-          controller: messageProvider.getUserInputTextController,
+          controller: textController,
         ),
       ),
     );
   }
 
-  Widget buildSendButton(MessageProvider messageProvider) {
+  Widget buildSendButton() {
     return FloatingActionButton(
       backgroundColor: Colors.transparent,
       elevation: 0,
@@ -128,21 +161,23 @@ class _ChatPageState extends State<ChatPage> {
       highlightElevation: 0,
       onPressed: () {
         //Check if the textfield has text or not
-        if (messageProvider.getUserInputText.isNotEmpty) {
-          socketService.sendMessage(messageProvider.getUserInputText);
+        if (textController.text.isNotEmpty) {
+          socketService.sendMessage(textController.text);
 
-          messageProvider.addMessage(
-              new Message('text', messageProvider.getUserInputText, null),
-              'user');
+          setState(() {
+            messages.add({
+              'message': new Message('text', textController.text, null),
+              'sender': 'user'
+            });
+          });
 
-          //textController.text = '';
-          messageProvider.setUserInputText('');
+          textController.text = '';
 
           focusNode.unfocus();
 
           //Scrolldown the list to show the latest message
-          messageProvider.getScrollController.animateTo(
-            messageProvider.getScrollController.position.maxScrollExtent,
+          scrollController.animateTo(
+            scrollController.position.maxScrollExtent,
             duration: Duration(milliseconds: 600),
             curve: Curves.ease,
           );
@@ -151,89 +186,334 @@ class _ChatPageState extends State<ChatPage> {
       child: Icon(
         Icons.send,
         size: 30,
-        color: messageProvider.getUserInputText == ''
-            ? Colors.black12
-            : Colors.black,
+        color: textController.text == '' ? Colors.black12 : Colors.black,
       ),
     );
   }
 
-  Widget buildInputArea(MessageProvider messageProvider) {
+  Widget buildInputArea() {
     return Container(
       width: width,
       color: Colors.transparent,
       child: Row(
         children: <Widget>[
-          buildChatInput(messageProvider),
-          buildSendButton(messageProvider),
+          buildChatInput(),
+          buildSendButton(),
         ],
       ),
     );
+  }
+
+  _configurationDialog(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return ConfigurationDialog();
+        });
   }
 
   @override
   Widget build(BuildContext context) {
     height = MediaQuery.of(context).size.height;
     width = MediaQuery.of(context).size.width;
-    return Consumer<MessageProvider>(
-        builder: (context, messageProvider, child) {
-      return Scaffold(
-        backgroundColor: Colors.white,
-        appBar: MainAppBar(messageProvider: messageProvider,),
-        body: GestureDetector(
-          onTap: () => focusNode.unfocus(),
-          child: Column(
-            children: <Widget>[
-              Expanded(
-                  child: ListView.builder(
-                controller: messageProvider.getScrollController,
-                itemCount: messageProvider.getLength,
-                itemBuilder: (BuildContext context, int index) {
-                  return buildMessage(index, messageProvider);
-                },
-              )),
-              buildInputArea(messageProvider),
-              !focusNode.hasFocus
-                  ? SizedBox(
-                      height: 20,
-                      child: Container(
-                        color: Colors.white,
-                      ),
-                    )
-                  : Container(),
-            ],
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(
+            Icons.settings,
+            color: Colors.black,
           ),
+          onPressed: () => _configurationDialog(context),
         ),
-      );
-    });
+        actions: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(right: 15.0),
+            child: Icon(
+              Icons.brightness_1,
+              color: true ? Colors.green : Colors.red,
+              size: 10,
+            ),
+          )
+        ],
+        backgroundColor: Colors.white,
+        title: Image(
+          image: AssetImage('assets/images/logo.png'),
+          width: 200,
+        ),
+      ),
+      body: GestureDetector(
+        onTap: () => focusNode.unfocus(),
+        child: Column(
+          children: <Widget>[
+            Expanded(
+                child: ListView.builder(
+              controller: scrollController,
+              itemCount: messages.length,
+              itemBuilder: (BuildContext context, int index) {
+                return buildMessage(index);
+              },
+            )),
+            buildInputArea(),
+            !focusNode.hasFocus
+                ? SizedBox(
+                    height: 20,
+                    child: Container(
+                      color: Colors.white,
+                    ),
+                  )
+                : Container(),
+          ],
+        ),
+      ),
+    );
   }
 
-  Widget buildMessage(int index, MessageProvider messageProvider) {
+  Widget buildMessage(int index) {
     Widget messageWidget;
-
-    List messages = messageProvider.getMessages;
 
     String sender = messages[index]['sender'];
     Message message = messages[index]['message'];
 
     switch (message.type) {
       case 'text':
-        messageWidget = textMessage(index, sender, message.text, messageProvider);
+        messageWidget = textMessage(index, sender, message.text);
         break;
       case 'quick_replies':
         messageWidget = quickRepliesMessage(
-            index, message.data, message.text, socketService, messageProvider);
+            index, message.data, message.text, socketService);
         break;
       case 'image_attachment':
         messageWidget = imageMessage(index, message.text);
         break;
       case 'gallery':
-        messageWidget =
-            galleryMessage(index, message.data, socketService, messageProvider);
+        messageWidget = galleryMessage(index, message.data, socketService);
         break;
       case 'video':
         break;
     }
     return messageWidget;
+  }
+
+// method to open a url
+  _launchUrl(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  Widget textMessage(int index, String sender, String text) {
+    if (text == null) return Container();
+
+    return GestureDetector(
+      onTap: () => sender == 'user' ? textController.text = text : null,
+      child: Container(
+        alignment:
+            sender == 'bot' ? Alignment.centerLeft : Alignment.centerRight,
+        child: Container(
+          padding: const EdgeInsets.all(20.0),
+          margin: const EdgeInsets.only(
+              top: 10, bottom: 10.0, left: 20.0, right: 20.0),
+          decoration: BoxDecoration(
+            color: sender == 'bot' ? Colors.grey[600] : Colors.grey[200],
+            border: null,
+            borderRadius: BorderRadius.circular(30.0),
+          ),
+          child: Text(
+            text,
+            style: TextStyle(
+                color: sender == 'bot' ? Colors.white : Colors.grey[900],
+                fontSize: 15.0),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget quickRepliesMessage(
+      int index, quickReplies, String text, SocketService socketService) {
+    List<Widget> quickReplyWidgets = List<Widget>();
+    // build quick replies
+    for (var qr in quickReplies) {
+      quickReplyWidgets.add(Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: OutlineButton(
+          shape: new RoundedRectangleBorder(
+              borderRadius: new BorderRadius.circular(30.0)),
+          padding: EdgeInsets.all(10.0),
+          child: Text(qr['title']),
+          onPressed: () {
+            socketService.sendMessage(qr['payload']);
+
+            setState(() {
+              messages.add({
+                'message': (new Message('text', qr['title'], null)),
+                'sender': 'bot'
+              });
+            });
+
+            //Scrolldown the list to show the latest message
+            scrollController.animateTo(
+              scrollController.position.maxScrollExtent,
+              duration: Duration(milliseconds: 600),
+              curve: Curves.ease,
+            );
+          },
+        ),
+      ));
+    }
+
+    return Container(
+      alignment: Alignment.centerLeft,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(20.0),
+            margin: const EdgeInsets.only(
+                top: 10, bottom: 10.0, left: 20.0, right: 20.0),
+            decoration: BoxDecoration(
+              color: Colors.grey[600],
+              border: null,
+              borderRadius: BorderRadius.circular(30.0),
+            ),
+            child: Text(
+              text,
+              style: TextStyle(color: Colors.white, fontSize: 15.0),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 10.0),
+            child: Wrap(children: quickReplyWidgets),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget imageMessage(int index, String url) {
+    return Container(
+      alignment: Alignment.centerLeft,
+      child: Container(
+          margin: const EdgeInsets.only(
+              top: 10, bottom: 10.0, left: 20.0, right: 50.0),
+          child: ClipRRect(
+            child: Image.network(url),
+            borderRadius: BorderRadius.circular(10.0),
+          )),
+    );
+  }
+
+  Widget galleryMessage(int index, List elements, SocketService socketService) {
+    List<Widget> galleryButtons = List<Widget>();
+    // build gallery buttons
+    for (var e in elements) {
+      for (var b in e['buttons'])
+        galleryButtons.add(FlatButton(
+          child: Text(
+            b['title'].toUpperCase(),
+            style: TextStyle(color: Colors.black),
+          ),
+          onPressed: () {
+            switch (b['type']) {
+              case 'postback':
+                socketService.sendMessage(b['payload']);
+
+                setState(() {
+                  messages.add({
+                    'message': (new Message('text', b['title'], null)),
+                    'sender': 'bot'
+                  });
+                });
+                break;
+              case 'web_url':
+                _launchUrl(b['url']);
+            }
+
+            //Scrolldown the list to show the latest message
+            scrollController.animateTo(
+              scrollController.position.maxScrollExtent,
+              duration: Duration(milliseconds: 600),
+              curve: Curves.ease,
+            );
+          },
+        ));
+    }
+
+    return Container(
+        alignment: Alignment.centerLeft,
+        margin: EdgeInsets.symmetric(vertical: 20.0),
+        height: 330,
+        child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: elements.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                padding: EdgeInsets.symmetric(horizontal: 10.0),
+                width: MediaQuery.of(context).size.width,
+                child: Card(
+                    color: Colors.white,
+                    child: Column(
+                      children: <Widget>[
+                        SizedBox(
+                          height: 250.0,
+                          child: Stack(
+                            children: <Widget>[
+                              Positioned.fill(
+                                  child: Container(
+                                color: Colors.black,
+                                child: Opacity(
+                                  opacity: 0.5,
+                                  child: Image.network(
+                                      elements[index]['image_url'],
+                                      fit: BoxFit.cover),
+                                ),
+                              )),
+                              Positioned(
+                                bottom: 0.0,
+                                left: 16.0,
+                                right: 16.0,
+                                child: FittedBox(
+                                    fit: BoxFit.scaleDown,
+                                    alignment: Alignment.centerLeft,
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: <Widget>[
+                                        Text(
+                                          elements[index]['title'],
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.w700,
+                                              fontSize: 20),
+                                        ),
+                                        Text(
+                                          elements[index]['subtitle'],
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.w600,
+                                              fontSize: 15),
+                                        )
+                                      ],
+                                    )),
+                              )
+                            ],
+                          ),
+                        ),
+                        ButtonBarTheme(
+                          data: ButtonBarThemeData(),
+                          child: ButtonBar(
+                            buttonPadding: EdgeInsets.symmetric(horizontal: 10),
+                            alignment: MainAxisAlignment.end,
+                            children: galleryButtons,
+                          ),
+                        )
+                      ],
+                    )),
+              );
+            }));
   }
 }
