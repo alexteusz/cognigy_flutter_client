40ebb5dfc2c5a36a09ae8ac65df4395fa356ab3f
Alexander Teusz <teusz.alexander@gmail.com>
2020-04-14 21:40:05 +0200

    added text parameter for custom text in push notification

--- lib/helper/notification_helper.dart
+++ lib/helper/notification_helper.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-Future<void> showNotification(
+Future<void> showNotification( String text,
     FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin) async {
   var androidPlatformChannelSpecifics = AndroidNotificationDetails(
       'your channel id', 'your channel name', 'your channel description',
@@ -9,7 +9,7 @@ Future<void> showNotification(
   var platformChannelSpecifics = NotificationDetails(
       androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
   await flutterLocalNotificationsPlugin.show(0, 'New Message:',
-      'Hi dude, this is a new message from the bot.', platformChannelSpecifics,
+      text, platformChannelSpecifics,
       payload: 'item x');
 }
 
